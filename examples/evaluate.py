0b50c536ed5d4cc0bd3e005167570bfd4e97a5ed
leon <leonhe0119@gmail.com>
2024-09-13 11:23:15 +0800

    feat(eval) ..

--- examples/evaluate.py
+++ examples/evaluate.py
@@ -22,7 +22,7 @@ right_arm = build_right_arm()
 right_arm.move_start_position(master=False)
 camera = CameraGroup()
 
-pretrained_policy_path = "../outputs/train/example_pusht_diffusion"
+pretrained_policy_path = "../outputs/train/grip_cube"
 assert os.path.exists(pretrained_policy_path)
 policy = DiffusionPolicy.from_pretrained(pretrained_policy_path)
 policy.eval()
@@ -53,24 +53,27 @@ while not done:
     # Prepare observation for the policy running in Pytorch
     start = time.time()
     puppet_state = right_arm.read_puppet_state()
-    img = camera.read_right()
     state = torch.tensor(puppet_state, dtype=torch.float32)
-    image = torch.from_numpy(img)
-    image = torch.einsum('h w c -> c h w', image)
-    image = image.to(torch.float32)
+
+    img = camera.read(['TOP', 'RIGHT'])
+    img_top = torch.from_numpy(img["TOP"])
+    img_top = torch.einsum('h w c -> c h w', img_top).to(torch.float32).unsqueeze(0)
+    img_right = torch.from_numpy(img["RIGHT"])
+    img_right = torch.einsum('h w c -> c h w', img_right).to(torch.float32).unsqueeze(0)
 
     # Send data tensors from CPU to GPU
     state = state.to(device, non_blocking=True)
-    image = image.to(device, non_blocking=True)
+    img_top = img_top.to(device, non_blocking=True)
+    img_right = img_right.to(device, non_blocking=True)
 
     # Add extra (empty) batch dimension, required to forward the policy
     state = state.unsqueeze(0)
-    image = image.unsqueeze(0)
 
     # Create the policy input dictionary
     observation = {
         "observation.state": state,
-        "observation.image": image,
+        "observation.images.right": img_right,
+        "observation.images.top": img_top
     }
 
     # Predict the next action with respect to the current observation
@@ -80,9 +83,9 @@ while not done:
     # Prepare the action for the environment
     numpy_action = action.squeeze(0).to("cpu").numpy()
 
-    fps_wait(15, start)
-    print("OUT:", numpy_action)
+    fps_wait(10, start)
     bit_width = 1 / (time.time() - start) / 2
+    print("OUT:", numpy_action, bit_width)
     right_angle, right_grasper = numpy_action[:6], numpy_action[6]
     right_arm.puppet.move_to(right_angle, bit_width)
     right_arm.grasper.set_angle(right_grasper)
