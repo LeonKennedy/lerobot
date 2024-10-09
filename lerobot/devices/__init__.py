e9b0897b101a9e353eba47f8065c88ea97e4c75a
LeonKennedy <lionhe0119@hotmail.com>
2024-10-09 14:54:45 +0800

    feat(robot) add robot module

--- lerobot/devices/__init__.py
+++ lerobot/devices/__init__.py
@@ -1,3 +1,4 @@
 from .main import build_two_grasper, build_left_grasper, build_right_grasper
 from .camera import CameraGroup
 from .arm import build_two_arm, build_left_arm, build_right_arm, Arm
+from .robot import Robot, build_robot
